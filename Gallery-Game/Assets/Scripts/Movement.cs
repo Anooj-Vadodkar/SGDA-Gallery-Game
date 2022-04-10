a07db56c763c2b8383edfe119d8a2875139b5de6
Anooj-Vadodkar <anoojrv@gmail.com>
2022-04-10 14:28:54 -0700

    Basic movement completed

---
+++ Gallery-Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    float x;
+    float z;
+    float speed = 10;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public float jumpHeight = 10;
+    public Transform groundCheck;
+    public float groundDistance = 1f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        
+        controller.Move(move*speed*Time.deltaTime);
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
+
